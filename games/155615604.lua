cca87c117e034f30cd0f7a0babfac8b3e64a7868
Seasonal <99521865+SeasonalKirito@users.noreply.github.com>
2024-09-17 18:00:50 -0700

    Add files via upload

--- games/155615604.lua
+++ games/155615604.lua
@@ -1,1 +1,56 @@
-print("Hello World!")
+local plrs = game.Players
+local lp = plrs.LocalPlayer
+
+function hit_player(player)
+    local args = {
+        [1] = game:GetService("Players")[player]
+    }
+    game:GetService("ReplicatedStorage").meleeEvent:FireServer(unpack(args))
+end
+
+function start_kill()
+    task.wait(0.1)
+    for _, v in pairs(plrs:GetPlayers()) do
+        if v ~= lp then
+            if v.Team ~= "Neutral" then
+                if lp.Character.Humanoid.Health > 0 then
+                    while v.Character and v.Character:FindFirstChild("Humanoid") and v.Character.Humanoid.Health > 0 do
+                        workspace.CurrentCamera.CameraSubject = v.Character.Head
+                        lp.Character.HumanoidRootPart.CFrame = v.Character.HumanoidRootPart.CFrame * CFrame.new(0, -7.5, 0)
+                        hit_player(v.Name)
+                        print("Hit: "..v.Name)
+                        task.wait(0.005)
+                    end
+                else
+                    repeat until lp.Character.Humanoid.Health > 0
+                end
+            end
+        end
+    end
+end
+
+local engine = loadstring(game:HttpGet("https://raw.githubusercontent.com/Singularity5490/rbimgui-2/main/rbimgui-2.lua"))()
+
+local window1 = engine.new({
+    text = "Darko | Prison Life | "..game.PlaceId,
+    
+    size = UDim2.new(300, 200),
+})
+
+window1.open()
+
+local main = window1.new({
+    text = "Main",
+})
+
+local kill_all = main.new("switch", {
+    text = "Kill All";
+})
+kill_all.set(false)
+kill_all.event:Connect(function(bool)
+    while bool do
+        
+    end
+end)
+
+print("Done")
