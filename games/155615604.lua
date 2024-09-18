181d06e43227983d87e667dbafb406aeee84c3ac
Seasonal <99521865+SeasonalKirito@users.noreply.github.com>
2024-09-17 18:14:14 -0700

    Add files via upload

--- games/155615604.lua
+++ games/155615604.lua
@@ -1,5 +1,6 @@
 local plrs = game.Players
 local lp = plrs.LocalPlayer
+local kill_all_active = false
 
 function hit_player(player)
     local args = {
@@ -11,10 +12,12 @@ end
 function start_kill()
     task.wait(0.1)
     for _, v in pairs(plrs:GetPlayers()) do
+        if not kill_all_active then return end
         if v ~= lp then
             if v.Team ~= "Neutral" then
                 if lp.Character.Humanoid.Health > 0 then
                     while v.Character and v.Character:FindFirstChild("Humanoid") and v.Character.Humanoid.Health > 0 do
+                        if not kill_all_active then return end
                         workspace.CurrentCamera.CameraSubject = v.Character.Head
                         lp.Character.HumanoidRootPart.CFrame = v.Character.HumanoidRootPart.CFrame * CFrame.new(0, -7.5, 0)
                         hit_player(v.Name)
@@ -22,7 +25,7 @@ function start_kill()
                         task.wait(0.005)
                     end
                 else
-                    repeat until lp.Character.Humanoid.Health > 0
+                    repeat until lp.Character.Humanoid.Health > 0 or not kill_all_active
                 end
             end
         end
@@ -48,8 +51,12 @@ local kill_all = main.new("switch", {
 })
 kill_all.set(false)
 kill_all.event:Connect(function(bool)
-    while bool do
+    kill_all_active = bool
+    if bool then
         start_kill()
+    else
+        workspace.CurrentCamera.CameraSubject = lp.Character.Humanoid
+        lp.Character.Humanoid.Health = 0
     end
 end)
 
