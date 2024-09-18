30ff3f7a589330375fadaf3630a5d4f0c54ec750
Seasonal <99521865+SeasonalKirito@users.noreply.github.com>
2024-09-17 18:09:32 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,20 @@
+repeat
+    task.wait()
+until game:IsLoaded()
 
+local placeID = game.PlaceId
+local github = "https://raw.githubusercontent.com/DarkoHub/DarkoHub/main/"
+local supported = github.."supported.lua"
+
+local gamesString = game:HttpGet(supported)
+local games = loadstring(gamesString)()
+
+for _, v in pairs(games) do
+    if v == placeID then
+        loadstring(game:HttpGet(github.."games/"..placeID..".lua", true))()
+        found = true
+        break
+    else
+        loadstring(game:HttpGet(github.."universal.lua", true))()
+    end
+end
